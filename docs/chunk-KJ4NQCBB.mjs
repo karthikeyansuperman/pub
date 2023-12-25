0968cc2ecb993aff9a3325c866c7da34fa57b0d0
karthikeyansuperman <124020337+karthikeyansuperman@users.noreply.github.com>
2023-12-25 10:29:19 +0530

    Delete docs directory

--- docs/chunk-KJ4NQCBB.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import"./chunk-HS54PXOJ.mjs";import"./chunk-3SZVCSKE.mjs";import"./chunk-LK223424.mjs";
--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-2GUETPWR.mjs";import"./chunk-CILJJG2B.mjs";import"./chunk-HS54PXOJ.mjs";import"./chunk-3SZVCSKE.mjs";import"./chunk-LK223424.mjs";export{a as default};
